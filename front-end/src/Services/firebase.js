e592e526887c716a484843287a84c70d3407cb22
Gabriel Eduardo <gabrieleduardo.s.rosa@gmail.com>
2022-06-24 21:20:29 -0300

    implementa firebase e styled components

---
+++ front-end/src/Services/firebase.js
@@ -0,0 +1,18 @@
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore'
+import { getAuth } from 'firebase/auth'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDTWG-MLjhmr7YKpeiAOBSVxeXlBT9h6s0",
+  authDomain: "chat-groisa.firebaseapp.com",
+  projectId: "chat-groisa",
+  storageBucket: "chat-groisa.appspot.com",
+  messagingSenderId: "1012331563027",
+  appId: "1:1012331563027:web:aae55574018c5c0c63d7e6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app)
+export const auth = getAuth(app)
